8c99f4946e571431315bc08d49db50c15bd50fd1
Simon Haydon <shaydon@koimo.net>
2026-01-09 14:30:14 +0000

    Implement /duplicate/~/* creation with preview
    Use visual editing machinery to preview an "archetype" page and allow saving under a new name.

--- packages/@tinacms/app/src/preview.tsx
+++ packages/@tinacms/app/src/preview.tsx
@@ -7,21 +7,25 @@ import { useGraphQLReducer } from './lib/graphql-reducer';
 
 type Config = Parameters<typeof defineConfig>[0];
 
-export const Preview = (
-  props: Config & {
-    url: string;
-    iframeRef: React.MutableRefObject<HTMLIFrameElement>;
-  }
-) => {
-  useGraphQLReducer(props.iframeRef, props.url);
+export const Preview = ({
+  url,
+  iframeRef,
+  action = 'edit',
+  ...config
+}: Config & {
+  url: string;
+  iframeRef: React.MutableRefObject<HTMLIFrameElement>;
+  action: 'edit' | 'duplicate';
+}) => {
+  useGraphQLReducer(iframeRef, url, action);
 
   return (
     <iframe
       data-test='tina-iframe'
       id='tina-iframe'
-      ref={props.iframeRef}
+      ref={iframeRef}
       className='h-screen w-full bg-white'
-      src={props.url}
+      src={url}
     />
   );
 };
--- packages/tinacms/src/toolkit/form-builder/index.ts
+++ packages/tinacms/src/toolkit/form-builder/index.ts
@@ -8,4 +8,5 @@ export * from './loading-dots';
 export * from './reset-form';
 export * from './form-actions';
 export * from './create-branch-modal';
+export * from './document-creation';
 export type { FieldRenderProps } from 'react-final-form';
